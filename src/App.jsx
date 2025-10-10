92901d28b957503a3427862f01cbee386a50eaf5
ilyafltv <keebro30@gmail.com>
2025-10-10 14:48:46 +0300

    feat: added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import HistoryPage from "./Pages/HistoryPage/index.jsx";
+import CalculatorPage from "./Pages/CalculatorPage/index.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <CalculatorPage />,
+  },
+  {
+    path: "/history",
+    element: <HistoryPage />,
+  },
+]);
+
 export default function App() {
-  return <div>React Calculator</div>;
+  return <RouterProvider router={router} />;
 }
---
+++ src/pages/CalculatorPage/index.jsx
@@ -0,0 +1,16 @@
+import Screen from "./components/Screen";
+import Keyboard from "./components/Keyboard";
+import Button from "../../components/Button";
+
+const CalculatorPage = () => {
+  return (
+    <div>
+      CalculatorPage
+      <Screen />
+      <Keyboard />
+      <Button />
+    </div>
+  );
+};
+
+export default CalculatorPage;
